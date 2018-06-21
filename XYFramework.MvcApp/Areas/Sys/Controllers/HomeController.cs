c79b5d16fd88aba089d429d003fe8c53ef99f6a4
wuchaozhe <468004901@qq.com>
2018-06-21 14:48:17 +0800

    add solution

---
+++ XYFramework.MvcApp/Areas/Sys/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XYFramework.MvcApp.Areas.Sys.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Sys/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
